77261865a265806b5cd98390079ed37f964cc17f
Stéphane Monfort <stephane.monfort@cacd2.fr>
2024-02-06 20:51:29 +0100

    Add log from fork

--- packages/cli/src/commands/migrations/run.ts
+++ packages/cli/src/commands/migrations/run.ts
@@ -61,6 +61,7 @@ export default class Command extends CmaClientCommand<typeof Command.flags> {
     runMigrationScripts: string[];
   }> {
     this.requireDatoProfileConfig();
+    this.log('Run from CACD2 fork');
 
     const preference = this.datoProfileConfig?.migrations;
 
